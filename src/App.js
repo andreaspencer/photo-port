780773d41066d210c000c52c49ccd9881ea4e9f6
andreaspencer <andreaaddawn@gmail.com>
2022-01-07 17:45:07 -0800

    click and blur

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
 import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -23,16 +24,25 @@ function App() {
     }
 ]);
 const [currentCategory, setCurrentCategory] = useState(categories[0]);
+const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
+      {!contactSelected ? (
+      <>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
+      </>
+      ) : (
+        <ContactForm></ContactForm>
+      )}
       </main>
     </div>
   );
